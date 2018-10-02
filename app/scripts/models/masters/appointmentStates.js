2bfb26645bcc3ed000eef0df4854c64d59585df5
Veronica Velez <verovelez1706@gmail.com>
2018-10-01 19:07:48 -0500

    se eliminan los archivos desktop.ini

--- app/scripts/models/masters/appointmentStates.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class EstadosVisitas{
-    constructor(idEstadoVisita = '', nombreEstadoVisita = '', cronogramasList = []){
-        this.idEstadoVisita = idEstadoVisita;
-        this.nombreEstadoVisita = nombreEstadoVisita;
-        this.cronogramasList = cronogramasList;
-    }
-}
--- app/scripts/models/masters/pettyCashStates.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class EstadosCajaMenor{
-    constructor(idEstadoCajaMenor = '', nombreEstado = '', detallesFlujosCajasMenoresList = []){
-        this.idEstadoCajaMenor = idEstadoCajaMenor;
-        this.nombreEstado = nombreEstado;
-        this.detallesFlujosCajasMenoresList = detallesFlujosCajasMenoresList;
-    }
-}
